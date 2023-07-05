16fe2e776e5d88b87b19403aebb9eca723cf8b38
kvishnu-1321 <vishnuvardhan.k@9and9.com>
2023-07-05 19:16:00 +0530

    responsive

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -33,7 +33,7 @@ function Inputs({ setQuery, units, setUnits }) {
   };
 
   return (
-    <div className="flex flex-row justify-center my-6">
+    <div className="flex flex-row  justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
           value={city}
